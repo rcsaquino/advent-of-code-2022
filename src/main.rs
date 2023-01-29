418cdb0d06c2e396b2d6f752f2a9056a6f5427cc
rcsaquino <rcsaquino.md@gmail.com>
2023-01-29 21:16:46 +0800

    added day 3

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod solutions;
 fn main() {
     solutions::day_one::print_answer(fs::read_to_string("./src/inputs/day_one.txt").unwrap());
     solutions::day_two::print_answer(fs::read_to_string("./src/inputs/day_two.txt").unwrap());
+    solutions::day_three::print_answer(fs::read_to_string("./src/inputs/day_three.txt").unwrap());
 }
---
+++ src/solutions/day_three.rs
@@ -0,0 +1,51 @@
+pub fn print_answer(input: String) {
+    // Part 1
+    let mut priority_sum: u32 = 0;
+    input.split("\r\n").for_each(|rucksack| {
+        let (first, second) = rucksack.split_at(rucksack.len() / 2);
+
+        for c in first.chars() {
+            if second.contains(c) {
+                priority_sum += get_priority(c);
+                break;
+            }
+        }
+    });
+    println!("Day 3 (Part 1): {priority_sum}");
+
+    // Part 2
+    priority_sum = 0;
+    let rucksacks: Vec<&str> = input.split("\r\n").collect();
+
+    for i in (0..rucksacks.len()).step_by(3) {
+        for c in rucksacks[i].chars() {
+            if rucksacks[i + 1].contains(c) && rucksacks[i + 2].contains(c) {
+                priority_sum += get_priority(c);
+                break;
+            }
+        }
+    }
+	println!("Day 3 (Part 2): {priority_sum}");
+}
+
+fn get_priority(letter: char) -> u32 {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".to_string();
+    let lower_case_vec: Vec<char> = alphabet.chars().collect();
+    let upper_case_vec: Vec<char> = alphabet.to_uppercase().chars().collect();
+
+    let priority;
+    if lower_case_vec.contains(&letter) {
+        priority = lower_case_vec
+            .iter()
+            .position(|character| character == &letter)
+            .unwrap()
+            + 1;
+    } else {
+        priority = upper_case_vec
+            .iter()
+            .position(|character| character == &letter)
+            .unwrap()
+            + 27;
+    }
+    return priority as u32;
+}
