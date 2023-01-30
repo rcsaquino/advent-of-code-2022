fba90a93eaf937aa26e5aa30ab32f8c0c529aa4d
rcsaquino <rcsaquino.md@gmail.com>
2023-01-31 00:11:23 +0800

    formatting changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 mod solutions;
+
 fn main() {
     solutions::day_01::print_answer(fs::read_to_string("./src/inputs/day_01.txt").unwrap());
     solutions::day_02::print_answer(fs::read_to_string("./src/inputs/day_02.txt").unwrap());
